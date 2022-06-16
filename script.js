b72618f2de3182b01eae7528073b6faa50a734cc
anamariadanila <ana.danila@info.uaic.ro>
2022-06-16 19:27:56 +0300

    rolling the dice

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+//selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
+
+//strating conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //generating the random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `img/dice-${dice}.png`;
+
+  //check for rolled 1
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    //switch to next player
+  }
+});
